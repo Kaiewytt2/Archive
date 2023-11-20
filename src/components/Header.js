1be1cb9aa0d5cc173cb97c2e435fd0cbe6bda165
Kaiewytt2 <sn.litvinov.28@gmail.com>
2023-11-20 17:31:47 +0700

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const Header = () => {
+    return (
+        <header>
+        <div className={"container mx-auto header-body"}>
+         <img className={"logo"} alt={"logo"} src={"/photo_2023-11-11_20-40-55.png"}/>
+        </div>
+        </header>
+    );
+};
+
